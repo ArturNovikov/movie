999946c65d6af0e087c530b32cf1094969f7ca4e
Artur Novikov <art14ek@tut.by>
2023-09-06 16:46:21 +0300

    genres repair v.1.0.

--- src/components/movie-bord/movie-bord.js
+++ src/components/movie-bord/movie-bord.js
@@ -12,8 +12,9 @@ export default class MovieBord extends Component {
         super(props);
         this.state = {
             movies: [],
+            genres: [],
         };
-    };
+    }
 
     componentDidMount() {
         movieService.getAllMovies()
@@ -21,26 +22,45 @@ export default class MovieBord extends Component {
                 this.setState({
                     movies: data.results
                 })
-                console.log(data)
+                console.log(data.results)
             })
             .catch(error => console.error(error));
-
+        
+        movieService.getGenres()
+            .then(data =>{
+                this.setState({
+                    genres: data.genres
+                })
+                console.log(data.genres)
+            })
+            .catch(error => console.log(error))
     };
 
+    getGenreNames(genreIds) {
+        const { genres } = this.state;
+        if (!genreIds || !genres) return [];
+        return genreIds.map(id => genres.find(genre => genre.id === id)?.name || '').filter(name => name);
+    }
+    
+
     render() {
-
-        const { movies } = this.state;
-
-        return(
+        const { movies, genres } = this.state;
+    
+        if (!movies.length || !genres.length) {
+            return <div>Loading...</div>;
+        }
+    
+        return (
             <div className="card-container">
                 <Row gutter={[36, 36]} justify="center">
-                {movies.map((movie, index) => (
-                    <Col key={ index } span={12}>
-                        <MovieItem movie={ movie } />
-                    </Col>
-                ))}
+                    {movies.map((movie, index) => (
+                        <Col key={index} span={12}>
+                            <MovieItem movie={movie} genres={this.getGenreNames(movie.genre_ids)} />
+                        </Col>
+                    ))}
                 </Row>
             </div>
         );
-    };
+    }
+    
 };
