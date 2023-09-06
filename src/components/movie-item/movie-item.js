cda0afa45d008b95aa0dc4b381c45c0fad3908d4
Artur Novikov <art14ek@tut.by>
2023-09-06 16:46:21 +0300

    genres repair v.1.0.

--- src/components/movie-item/movie-item.js
+++ src/components/movie-item/movie-item.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, Space, Tag } from 'antd';
 import './movie-item.css';
 
-const MovieItem = ({ movie }) => (
+const MovieItem = ({ movie, genres }) => (
     <Card hoverable style={{ width: 451 }}>
         <div style={{ display: 'flex' }}>
             <img
@@ -13,13 +13,15 @@ const MovieItem = ({ movie }) => (
             <div style={{ width: '50%', padding: '16px' }}>
                 <h3>{movie.title}</h3>
                 <p>{movie.release_date}</p>
+
                 <Space size={[0, 8]} wrap>
-                    {movie.genre_ids.map((genre) => (
-                        <Tag key={genre.id}>
-                            {genre.name}
+                    {genres && genres.map((genre, index) => (
+                        <Tag key={index}>
+                            {genre}
                         </Tag>
                     ))}
                 </Space>
+
                 <p>{movie.overview}</p>
             </div>
         </div>
--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -5,7 +5,7 @@
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzJkYWExNTU5ZTJhNDUzMjRmOWUxM2RkNGFkNGIwZCIsInN1YiI6IjY0Zjc3YTdiMmNkZTk4MDBhZDFjYzFjYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NUOLPt8dh97Wfh7pmBg96dJnjrAZYO9CwFIZl7eJL_8'
+        Authorization: null,
       }
     }
 
@@ -20,6 +20,10 @@
 
   getAllMovies() {
     return this.getResourse('https://api.themoviedb.org/3/search/movie?query=return&include_adult=false&language=en-US&page=1')
+  }
+
+  getGenres() {
+    return this.getResourse('https://api.themoviedb.org/3/genre/movie/list?language=en')
   }
 
 };
